e65e2fb7334903d17cc11b517620aa213af52257
Matthias Jakobs <matthias.jakobs@tu-dortmund.de>
2025-11-11 14:28:38 +0100

    added script for amrules

---
+++ run_amrules.sh
@@ -0,0 +1,6 @@
+python amrules.py --version 1 --method AMRules > logs/amrules_v1.log
+python amrules.py --version 2 --method AMRules > logs/amrules_v2.log
+python amrules.py --version 1 --method ChebyOS > logs/chebyos_v1.log
+python amrules.py --version 2 --method ChebyOS > logs/chebyos_v2.log
+python amrules.py --version 1 --method ChebyUS > logs/chebyus_v1.log
+python amrules.py --version 2 --method ChebyUS > logs/chebyus_v2.log
